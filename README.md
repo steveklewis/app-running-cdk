1a3cb9e0db65a634975c4d8c7be98a58f769d1a1
Steve Lewis <lewisste@argodata.com>
2023-12-28 21:06:46 -0600

    Get basic app runner working deployed via cdk.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # app-running-cdk
 app-running-cdk
+
+
+* https://www.youtube.com/watch?v=jOwQdXbmHC4
+** Here she is using the level 1 construct because the level 2 construct is in alpha and she wants to be able to do automatic deployments. She doesn't use a VPC with the app runner.
+
+*
+* 
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,47 @@
 import { App, Stack, StackProps } from 'aws-cdk-lib';
+// import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { CfnService } from 'aws-cdk-lib/aws-apprunner';
 import { Construct } from 'constructs';
+
+import * as config from '../config.json';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
     // define resources here...
+    // VPC
+    // const vpc = new ec2.Vpc(this, 'Vpc', {
+    //   ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16')
+    // });
+
+    // const vpcEndpoint = new ec2.VpcEndpoint(this, 'VpcEndpoint');
+
+    // const vpcConnector = new apprunner.VpcConnector(this, 'VpcConnector', {
+    //   vpc,
+    //   vpcSubnets: vpc.selectSubnets({ subnetType: ec2.SubnetType.PUBLIC }),
+    //   vpcConnectorName: 'MyVpcConnector',
+    // });
+
+
+    new CfnService(this, 'AppRunningService', {
+      sourceConfiguration: {
+        authenticationConfiguration: {
+          connectionArn: config.connectionArn
+        },
+        autoDeploymentsEnabled: false,
+        codeRepository: {
+          codeConfiguration: {
+            configurationSource: "REPOSITORY"
+          },
+          repositoryUrl: 'https://github.com/steveklewis/app-running',
+          sourceCodeVersion: {
+            type: 'BRANCH',
+            value: 'main',
+          },
+        },
+      },
+    });
   }
 }
 
